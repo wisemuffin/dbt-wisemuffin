a9bffe532bcead668ef910cc74837b53c7efa63f
David Griffiths <davidgg777@hotmail.com>
2021-10-29 08:38:41 +1100

    added some example exposures

--- models/marts/sales/fct_deal.sql
+++ models/marts/sales/fct_deal.sql
@@ -7,6 +7,10 @@ select
     service_sk,
     owner_sk,
     company_sk,
-    deal_id
+    deal_id,
     amount,
 from deal
+
+{% if target.name == 'dev' %}
+where created_at >= cast('2021-10-27' as timestamp)
+{% endif %}
